75b8ad7be8a3ebc3de30ab43149e4acc099c1fa9
Kyle Partridge <kyle.partridge@sendgrid.com>
2019-04-15 15:09:44 -0700

    remove the ROWS and COLS macros
    i am not using any stack allocated arrays for this structure, so it is not necessary to use the macro definitions.
    
    this should also make it easier to read these values from ncurses

--- main.c
+++ main.c
@@ -4,22 +4,19 @@
 
 #include "game_of_life.h"
 
-#define ROWS 20
-#define COLS 40
-
-void draw_board(int32 *board, int generation) {
+void draw_board(int32 *board, int rows, int cols, int generation) {
 	printw("Generation %d\n", generation);
 	printw("\n");
 
 	printw(" ");
-	for (int i = 0; i < COLS; i++) {
+	for (int i = 0; i < cols; i++) {
 		printw("_");
 	}
 	printw("\n");
-	for (int j = 0; j < ROWS; j++) {
+	for (int j = 0; j < rows; j++) {
 		printw("|");
-		for (int i = 0; i < COLS; i++) {
-			if (get_board_value(board, ROWS, COLS, i, j) == 1) {
+		for (int i = 0; i < cols; i++) {
+			if (get_board_value(board, rows, cols, i, j) == 1) {
 				printw("*");
 			} else {
 				printw(" ");
@@ -29,20 +26,20 @@ void draw_board(int32 *board, int generation) {
 	}
 
 	printw(" ");
-	for (int i = 0; i < COLS; i++) {
+	for (int i = 0; i < cols; i++) {
 		printw("_");
 	}
 	printw("\n");
 }
 
 
-void make_glider(int32 *board, int32 x, int32 y) {
+void make_glider(int32 *board, int rows, int cols, int32 x, int32 y) {
 	// everyone loves a glider
-	set_board_value(board, ROWS, COLS, x+0, y+1, 1);
-	set_board_value(board, ROWS, COLS, x+1, y+2, 1);
-	set_board_value(board, ROWS, COLS, x+2, y+0, 1);
-	set_board_value(board, ROWS, COLS, x+2, y+1, 1);
-	set_board_value(board, ROWS, COLS, x+2, y+2, 1);
+	set_board_value(board, rows, cols, x+0, y+1, 1);
+	set_board_value(board, rows, cols, x+1, y+2, 1);
+	set_board_value(board, rows, cols, x+2, y+0, 1);
+	set_board_value(board, rows, cols, x+2, y+1, 1);
+	set_board_value(board, rows, cols, x+2, y+2, 1);
 }
 
 int main() {
@@ -50,21 +47,27 @@ int main() {
 
 	int generation = 0;
 
-	int32 *board = malloc(ROWS*COLS*sizeof(int32));
-	int32 *board_2 = malloc(ROWS*COLS*sizeof(int32));
+	int rows = 20;
+	int cols = 40;
 
+	// this means would could make a board up to 10x the size
+	// that should be very sufficient
+	int extra = 10;
+	int32 *board = malloc(extra*rows*cols*sizeof(int32));
+	int32 *board_2 = malloc(extra*rows*cols*sizeof(int32));
+
+	// verify that the boards exist
 	assert(board != 0);
 	assert(board_2 != 0);
 
-	make_glider(board, 0, 0);
-	make_glider(board, 10, 10);
+	make_glider(board, rows, cols, 0, 0);
+	make_glider(board, rows, cols, 10, 10);
 
-
-  while (true) {
+	while (true) {
 		clear();
 
-		next_generation(board_2, board, ROWS, COLS);
-		draw_board(board, generation);
+		next_generation(board_2, board, rows, cols);
+		draw_board(board, rows, cols, generation);
 
 		usleep(100*1000);
 
@@ -75,9 +78,9 @@ int main() {
 		tmp = board_2;
 		board_2 = board;
 		board = tmp;
-  }
+	}
 
 	endwin();
 
-  return 0;
+	return 0;
 }

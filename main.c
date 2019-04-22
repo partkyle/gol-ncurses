05a92af343ae77e21c0c240a63f0094e453037a5
Kyle Partridge <kyle.partridge@sendgrid.com>
2019-04-22 16:35:35 -0700

    print using move for no reason

--- main.c
+++ main.c
@@ -5,31 +5,34 @@
 #include "game_of_life.h"
 
 void draw_board(int32 *board, int rows, int cols, int generation) {
-	printw("Generation %d\n", generation);
-	printw("\n");
-
-	printw(" ");
+	addch(ACS_ULCORNER);
 	for (int i = 0; i < cols; i++) {
-		printw("_");
+		addch(ACS_HLINE);
 	}
-	printw("\n");
+	addch(ACS_URCORNER);
 	for (int j = 0; j < rows; j++) {
-		printw("|");
+		move(j+1,0);
+
+		addch(ACS_VLINE);
 		for (int i = 0; i < cols; i++) {
 			if (get_board_value(board, rows, cols, i, j) == 1) {
-				printw("*");
+				addch('*');
 			} else {
-				printw(" ");
+				addch(' ');
 			}
 		}
-		printw("|\n");
+		addch(ACS_VLINE);
 	}
+	move(rows+1, 0);
 
-	printw(" ");
+	addch(ACS_LLCORNER);
 	for (int i = 0; i < cols; i++) {
-		printw("_");
+		addch(ACS_HLINE);
 	}
-	printw("\n");
+	addch(ACS_LRCORNER);
+
+	move(0, 0);
+	printw("Generation %d", generation);
 }
 
 
@@ -64,14 +67,21 @@ void copy_board(int32 *from, int from_rows, int from_cols, int32 *to, int to_row
 	}
 }
 
+void swap(int32 **a, int32 **b)
+{
+	int32 *tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
 int main() {
-	initscr();
+	WINDOW *window = initscr();
 	// allow looping over getch without blocking
 	timeout(1);
 
 	int generation = 0;
 
-	int rows = LINES - 5;
+	int rows = LINES - 2;
 	int cols = COLS - 4;
 
 	// this means would could make a board up to 10x the size
@@ -88,12 +98,12 @@ int main() {
 	make_glider(board, rows, cols, 10, 10);
 
 	int key;
-	while ((key = getch()) != 27) {
+	while ((key = wgetch(window)) != 27) {
 		if (key == KEY_RESIZE) {
 			int prev_rows = rows;
 			int prev_cols = cols;
 
-			rows = LINES - 5;
+			rows = LINES - 2;
 			cols = COLS - 4;
 
 			// reuse the previous generation to rebuild the board state at the new size
@@ -104,20 +114,18 @@ int main() {
 			continue;
 		}
 
-		clear();
-
 		next_generation(board_2, board, rows, cols);
+
+		clear();
 		draw_board(board, rows, cols, generation);
 
 		usleep(100*1000);
 
 		refresh();
+
 		generation++;
 
-		int *tmp;
-		tmp = board_2;
-		board_2 = board;
-		board = tmp;
+		swap(&board, &board_2);
 	}
 
 	endwin();
